a410746b82b2564a5611d7234bb39faa8c0da440
Randalix <j.heintz90@gmail.com>
2026-03-07 21:40:04 +0100

    fix i-mlv OpenMP linking on macOS
    The condition checked $(CXX) against "clang" which never matched since
    CXX is "clang++". Also Apple Clang doesn't support -fopenmp=libomp.
    Use findstring on $(CC) and link with -lomp instead.

--- src/pipe/modules/i-mlv/flat.mk
+++ src/pipe/modules/i-mlv/flat.mk
@@ -1,7 +1,6 @@
 pipe/modules/i-mlv/libi-mlv.so: pipe/modules/i-mlv/mlv.h pipe/modules/i-mlv/raw.h pipe/modules/i-mlv/video_mlv.c pipe/modules/i-mlv/video_mlv.h pipe/modules/i-mlv/liblj92/lj92.c
-ifeq ($(CXX),clang)
-MOD_LDFLAGS+=-fopenmp=libomp
-endif
-ifeq ($(CXX),g++)
+ifneq ($(findstring clang,$(CC)),)
+MOD_LDFLAGS+=-L/opt/homebrew/opt/libomp/lib -lomp
+else ifneq ($(findstring gcc,$(CC)),)
 MOD_LDFLAGS+=-lgomp
 endif
